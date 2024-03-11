3e18d195528d0f5c3171f8bacc1608bfb730fbc7
OHshajim <ajshajimmax@gmail.com>
2024-03-12 05:09:13 +0600

    handle to visited Country

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,6 +4,12 @@ import Country from "../country/Country";
 import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([])
+    // const [country, setCountry] = useState([]) 
+  
+    function handleVisitedCountry (country){
+        console.log(country);
+        console.log("visited");
+    }
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -16,7 +22,7 @@ const Countries = () => {
          <h3>Countries : {countries.length}</h3>
         <div className="container">
             {
-                countries.map((country) => <Country key={country.cca3} country={country}></Country>)
+                countries.map((country) => <Country key={country.cca3}   country={country} handleCountry={handleVisitedCountry}></Country>)
             }
         </div>
         </>
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
+const Country = ({ country },handleCountry) => {
     console.log(country);
     const { name, flags ,population,area ,cca3 } = country
     const [visit,setVisit] = useState(false)
     function handleVisit (){
         setVisit(!visit)
     }
+    console.log(handleCountry);
     return (
-        <div className='country'>
+        <div className={`country ${visit&&'visited'}`}>
             <img src={flags.png} alt={flags.alt} />
             {/* <img src={flags.svg} alt="" /> */}
             <h3>Name : {name.common}</h3>
@@ -16,7 +17,7 @@ const Country = ({ country }) => {
             <p>Population : {population} peoples</p>
             <p>Area : {area} Kilometer <sup>2</sup></p>
             <p>Code : {cca3}</p>
-            <button>Mark to visit </button><br />
+            {/* <button>Mark to visit </button><br /> */}
             <button onClick={handleVisit}>{visit? 'Visited':'visit'}</button>
         </div>
     );
